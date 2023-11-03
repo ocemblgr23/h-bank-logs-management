611cf757a856850658f76f44a923ed154e5ffd00
ocemblgr23 <131694566+ocemblgr23@users.noreply.github.com>
2023-11-03 12:30:29 +0530

    pincode api integreate

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 export interface ILogs {
   ko_code: string
   ko_name: string
   contact_no: string
   location: string
+  pincode:string
   district: string
   tl_name: string
   anydesk_id: string
@@ -12,6 +14,8 @@ export interface ILogs {
   create_at?: string
   update_at?: string
 }
+
+export  const API_PIN = "https://api.postalpincode.in/pincode/"
 
 
 
@@ -22,6 +26,8 @@ export interface ILogs {
 })
 export class HomeComponent implements OnInit{
 
+
+
   logs:ILogs={
     anydesk_id: "",
     contact_no: "",
@@ -30,11 +36,22 @@ export class HomeComponent implements OnInit{
     ko_code: "",
     ko_name: "",
     location: "",
+    pincode:"",
     tl_name: ""
+  }
+
+  constructor(private http:HttpClient) {
+
   }
 
   ngOnInit() {
 
+  }
+
+  checkPincode() {
+    this.http.get(`${API_PIN}${this.logs.pincode}`).subscribe((resp)=>{
+      console.log(resp)
+    })
   }
 
 }
