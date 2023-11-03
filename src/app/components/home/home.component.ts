323104af993656a2db0afba0d11b4a6189d24d6d
ocemblgr23 <131694566+ocemblgr23@users.noreply.github.com>
2023-11-03 11:54:16 +0530

    Implement modal for logs form

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+export interface ILogs {
+  ko_code: string
+  ko_name: string
+  contact_no: string
+  location: string
+  district: string
+  tl_name: string
+  anydesk_id: string
+  error_desc: string
+  create_at?: string
+  update_at?: string
+}
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  logs:ILogs={
+    anydesk_id: "",
+    contact_no: "",
+    district: "",
+    error_desc: "",
+    ko_code: "",
+    ko_name: "",
+    location: "",
+    tl_name: ""
+  }
+
+  ngOnInit() {
+
+  }
 
 }
--- src/app/prime/prime.module.ts
+++ src/app/prime/prime.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
   exports: [
     ButtonModule,
-    MenubarModule
+    MenubarModule,
+    InputTextModule
   ],
 })
 export class PrimeModule {}
